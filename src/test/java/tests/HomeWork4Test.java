7b1a0339a5059307a9d4193eaa741039b015b0f2
menand <menand@narod.ru>
2025-03-24 15:03:06 +0300

    hw4_done

---
+++ src/test/java/tests/HomeWork4Test.java
@@ -0,0 +1,27 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class HomeWork4Test {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void selenideGithubTest() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $$("#wiki-body ul li").findBy(text("Soft assertions")).shouldBe(visible);
+    }
+}
